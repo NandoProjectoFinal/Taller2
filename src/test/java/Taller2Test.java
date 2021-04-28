e5239afaf5566a5861b92b9b3fd2d6c41f07f301
NandoProjectoFinal <61599328+NandoProjectoFinal@users.noreply.github.com>
2021-04-28 09:08:32 -0400

    Toma 2

---
+++ src/test/java/Taller2Test.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Taller2Test {
+
+    @Test
+    void main() {
+        Taller2 palabra = new Taller2();
+        assertEquals("", "");
+    }
+}
